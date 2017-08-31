d1cc6cdb19073ae72815cd21251f89aa253b60bd
Kewaleede <kewalee.de.57@ubu.ac.th>
2017-08-31 11:06:38 +0700

    add Name&No.

--- nodejs-01/myfirst/index.js
+++ nodejs-01/myfirst/index.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1 style="color:red;background-color:black;">Hello World!</h1>');
+    res.end('<h1 style="color:red;background-color:black;">Hello World form Kewalee 5711403157!</h1>');
 }).listen(8080); 
